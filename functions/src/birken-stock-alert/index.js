806769fcf10a5c53a98a91a39acb95b87f3aa55f
Oscar Paramo <oscar@paramo.dev>
2021-04-27 23:13:42 -0500

    Refactor birkenstock function to run on schedule.

--- functions/src/birken-stock-alert/index.js
+++ functions/src/birken-stock-alert/index.js
@@ -13,50 +13,48 @@ const toSms = config.birkenstockalert.toSms;
 const client = require('twilio')(accountSid, authToken);
 
 const url = 'https://www.dsw.com/en/us/product/birkenstock-arizona-slide-sandal---womens/171995';
+const uA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0';
 const sizes = {
   'size\nEU 38 / US 7-7.5\nof Product Arizona Slide Sandal - Women\'s - Not Selected': true,
   'size\nEU 39 / US 8-8.5\nof Product Arizona Slide Sandal - Women\'s - Not Selected': true,
 };
 
-const birkenStockAlert = functions.https.onRequest(async (req, res) => {
-  const browser = await puppeteer.launch();
+const birkenStockAlert = functions.pubsub.schedule('every 3 hours')
+    .onRun(async (context) => {
+      const browser = await puppeteer.launch();
 
-  // set up page
-  const page = await browser.newPage();
-  await page.setViewport({width: 1280, height: 720});
-  await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-  );
-  await page.goto(url, {waitUntil: 'networkidle2'});
+      // set up page
+      const page = await browser.newPage();
+      await page.setViewport({width: 1280, height: 720});
+      await page.setUserAgent(uA);
+      await page.goto(url, {waitUntil: 'networkidle2'});
 
-  // click on desired color of shoes
-  await page.waitForSelector('img[alt="Arizona Slide Sandal - Women\'s Mocha view"]');
-  await page.click('img[alt="Arizona Slide Sandal - Women\'s Mocha view"]');
+      // click on desired color of shoes
+      await page.waitForSelector('img[alt="Arizona Slide Sandal - Women\'s Mocha view"]');
+      await page.click('img[alt="Arizona Slide Sandal - Women\'s Mocha view"]');
 
-  // get all the text on all the buttons
-  const buttonsText = await page.evaluate(() => {
-    const buttons = [...document.querySelectorAll('button')];
-    return buttons.map((button) => button.innerText);
-  });
+      // get all the text on all the buttons
+      const buttonsText = await page.evaluate(() => {
+        const buttons = [...document.querySelectorAll('button')];
+        return buttons.map((button) => button.innerText);
+      });
 
-  // filter the button text to see if the right sizes are available
-  const desiredSizes = buttonsText.filter((text) => sizes[text]);
+      // filter the button text to see if the right sizes are available
+      const desiredSizes = buttonsText.filter((text) => sizes[text]);
 
-  // send a text notification if the
-  if (desiredSizes.length > 0) {
-    console.log('shoes in size/color are in stock');
+      // send a text notification if the shoes are in stock
+      if (desiredSizes.length > 0) {
+        console.log('shoes in size/color are in stock');
 
-    const body = `Your birkenstocks... are in stock! 😍 P.S. I love you kibby 🐱\n${url}`;
+        const body = `Your birkenstocks... are in stock! 😍 P.S. I love you kibby 🐱\n${url}`;
 
-    await client.messages.create({body, from: fromSms, to: toSms});
-  } else {
-    console.log('shoes in size/color not in stock');
-  }
+        await client.messages.create({body, from: fromSms, to: toSms});
+      } else {
+        console.log('shoes in size/color not in stock');
+      }
 
-  await browser.close();
-
-  res.end();
-});
+      await browser.close();
+    });
 
 module.exports = birkenStockAlert;
 
